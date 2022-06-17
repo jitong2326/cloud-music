910df8b2919ffc051e72608d23112c25719d2171
wangz <jitong2326@163.com>
2022-06-17 17:57:51 +0800

    update

--- src/views/Found/Found.vue
+++ src/views/Found/Found.vue
@@ -18,11 +18,18 @@
         </SwiperSlide>
       </Swiper>
     </div>
+    <div class="nav">
+      <div v-for="e in 8" :key="e" class="nav-item">
+        <div class="nav-icon">
+          <svg-icon name="playlists" :color="themeColor"></svg-icon>
+        </div>
+        <span class="nav-item-title">歌单</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import { getBanner } from '@/api/found/index'
 
@@ -31,10 +38,14 @@ import 'swiper/css'
 import 'swiper/css/effect-cube'
 import 'swiper/css/pagination'
 import { onMounted, ref } from 'vue'
-
+import { useSettingStore } from '@/store/setting'
 
 interface IBanner {
   imageUrl: string
+  typeTitle: string
+  titleColor: string
+  targetType: number
+  targetId: bigint
 }
 
 let modules = [Navigation, Pagination, Scrollbar, A11y, Autoplay]
@@ -45,32 +56,76 @@ onMounted(() => {
     banners.value = res.data.banners
   })
 })
+
+const setting = useSettingStore()
+let themeColor = setting.cssVar.themeColor
+let themeColorWithOpacity = themeColor + '44'
 </script>
 
-<style lang="less" scoped>
+<style lang="less">
 .home-container {
   width: 100%;
   height: calc(100vh - 120px);
   box-sizing: border-box;
+  padding: 20px;
   overflow-y: scroll;
   h1 {
     color: @themeColor;
   }
   .banner {
     width: 100%;
-    height: 300px;
-    padding: 20px;
+    height: 340px;
     box-sizing: border-box;
   }
   .swiper {
     width: 100%;
-    height: 300px;
+    height: 100%;
     border-radius: 10px;
     overflow: hidden;
     .swiper-slide {
       img {
         width: 100%;
-        height: 300px;
+        height: 100%;
+      }
+    }
+  }
+  .nav {
+    display: flex;
+    margin-top: 30px;
+    overflow-x: scroll;
+    &::-webkit-scrollbar {
+      width: 0 !important;
+    }
+    &::-webkit-scrollbar {
+      width: 0 !important;
+      height: 0;
+    }
+    // background-color: blueviolet;
+    .nav-item {
+      padding: 10px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      margin: 0 12px;
+      .nav-icon {
+        width: 90px;
+        height: 90px;
+        border-radius: 50%;
+        background-color: v-bind(themeColorWithOpacity);
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 10px;
+      }
+      .icon-playlists {
+        width: 60px;
+        height: 60px;
+      }
+      .nav-item-title {
+        font-size: 12px;
+        color: @text-color-gray;
       }
     }
   }
